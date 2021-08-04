a005b2e0da1edcf16840a62336b54c795b86b1e4
shiika89 <kazuki_plant@yahoo.co.jp>
2021-08-04 20:07:10 +0900

    コミット

--- GameJam/Assets/Script/Ishii/TankPlayer.cs
+++ GameJam/Assets/Script/Ishii/TankPlayer.cs
@@ -5,6 +5,11 @@ using BaseSystem.Property;
 
 public class TankPlayer : BaseProp ,IPlayerProp
 {
+    private bool m_AttackStart = false;
+    private float m_time;
+    [SerializeField] float m_intarval;
+    public float Cost => 5f;
+
     private void Awake()
     {
         hp = 100;
@@ -13,17 +18,36 @@ public class TankPlayer : BaseProp ,IPlayerProp
 
     private void Update()
     {
-        
-    }
-
-    public void Attack()
-    {
-
+        if (m_AttackStart == true)
+        {
+            m_time += Time.deltaTime;
+        }
+        if (m_time > m_intarval)
+        {
+            // ‚±‚±‚ÉEnemy‚Éƒ_ƒ[ƒW‚ğ—^‚¦‚éˆ—‚ğ‘‚­
+            m_time = 0;
+        }
     }
 
     public void Dmage(int atk)
     {
         hp -= atk;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            m_AttackStart = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            m_AttackStart = false;
+        }
     }
 
     public void OnAtkChanged(int value)
