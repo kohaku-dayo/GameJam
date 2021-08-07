693a0b0c1d84ab9ce97a0d69f070537e80e20546
Goriyasu <goriyasu1998@gmail.com>
2021-08-07 17:23:51 +0900

    エネミーかぶり削除

--- GameJam/Assets/Script/Kohaku/Enemy.cs
+++ GameJam/Assets/Script/Kohaku/Enemy.cs
@@ -8,180 +8,180 @@ using System;
 using Cysharp.Threading.Tasks;
 using UnityEngine.UI;
 
-public class Enemy : EnemyProp, IEnemy
-{
-    [Header("Parameter")]
-    [SerializeField] int HP = 10;
-    [SerializeField] int ATK = 2;
-    [SerializeField] float m_speed;
-    [SerializeField] List<Collider> m_serchCollider;
-    [SerializeField] Slider m_slider;
-    [SerializeField] GameObject effect;
-    [SerializeField] int m_Cost;
-    [SerializeField] Animator m_anim;
-    private GameObject m_target;
-    private GameObject m_Tower;
-    private Rigidbody m_rigid;
-    private Vector3 m_targetpos;
-    bool isMovePleyer = false;
-    bool iscollider = false;
-    [SerializeField] float m_intarval = 3;
-    Vector3 thispos;
-    public int Attack { get; set; }
-    int m_maxHp;
-    private void Awake()
-    {
-        hp = HP;
-        m_maxHp = HP;
-        Attack = ATK;
-        m_rigid = GetComponent<Rigidbody>();
-        m_Tower = GameObject.FindGameObjectWithTag("Tower");
-        SetTrigger();
-        m_slider.maxValue = HP;
-        m_anim.SetBool("Walk", true);
+//public class Enemy : EnemyProp, IEnemy
+//{
+//    [Header("Parameter")]
+//    [SerializeField] int HP = 10;
+//    [SerializeField] int ATK = 2;
+//    [SerializeField] float m_speed;
+//    [SerializeField] List<Collider> m_serchCollider;
+//    [SerializeField] Slider m_slider;
+//    [SerializeField] GameObject effect;
+//    [SerializeField] int m_Cost;
+//    [SerializeField] Animator m_anim;
+//    private GameObject m_target;
+//    private GameObject m_Tower;
+//    private Rigidbody m_rigid;
+//    private Vector3 m_targetpos;
+//    bool isMovePleyer = false;
+//    bool iscollider = false;
+//    [SerializeField] float m_intarval = 3;
+//    Vector3 thispos;
+//    public int Attack { get; set; }
+//    int m_maxHp;
+//    private void Awake()
+//    {
+//        hp = HP;
+//        m_maxHp = HP;
+//        Attack = ATK;
+//        m_rigid = GetComponent<Rigidbody>();
+//        m_Tower = GameObject.FindGameObjectWithTag("Tower");
+//        SetTrigger();
+//        m_slider.maxValue = HP;
+//        m_anim.SetBool("Walk", true);
 
-    }
+//    }
 
-    void SetTrigger()
-    {
-        foreach (var c in m_serchCollider)
-        {
-            c.OnTriggerEnterAsObservable()
-                .Where(other => other.gameObject.tag == "Player")
-                .Subscribe(other =>
-                {
-                    m_target = other.gameObject;
-                    isMovePleyer = true;
-                    thispos = this.transform.position;
-                    m_targetpos = m_target.transform.position + (this.transform.position - m_target.transform.position).normalized;
-                });
+//    void SetTrigger()
+//    {
+//        foreach (var c in m_serchCollider)
+//        {
+//            c.OnTriggerEnterAsObservable()
+//                .Where(other => other.gameObject.tag == "Player")
+//                .Subscribe(other =>
+//                {
+//                    m_target = other.gameObject;
+//                    isMovePleyer = true;
+//                    thispos = this.transform.position;
+//                    m_targetpos = m_target.transform.position + (this.transform.position - m_target.transform.position).normalized;
+//                });
 
-            c.OnTriggerExitAsObservable()
-            .Where(other => other.gameObject.tag == "Player")
-            .Subscribe(other =>
-            {
-                //m_target = other.gameObject;
-                Debug.Log("Exit");
-                isMovePleyer = false;
+//            c.OnTriggerExitAsObservable()
+//            .Where(other => other.gameObject.tag == "Player")
+//            .Subscribe(other =>
+//            {
+//                //m_target = other.gameObject;
+//                Debug.Log("Exit");
+//                isMovePleyer = false;
 
-            });
-        }
-    }
+//            });
+//        }
+//    }
 
-    float attackTime;
+//    float attackTime;
    
-    private void Update()
-    {
+//    private void Update()
+//    {
   
-        if (!isMovePleyer || m_target == null)
-        {
-            Debug.Log("ChaseTower");
+//        if (!isMovePleyer || m_target == null)
+//        {
+//            Debug.Log("ChaseTower");
 
-            m_rigid.velocity = (m_Tower.transform.position - this.transform.position).normalized * m_speed;
-            //transform.position += (m_targetpos - this.transform.position).normalized * m_speed * Time.deltaTime;
+//            m_rigid.velocity = (m_Tower.transform.position - this.transform.position).normalized * m_speed;
+//            //transform.position += (m_targetpos - this.transform.position).normalized * m_speed * Time.deltaTime;
 
-        }
-        else
-        {
-            if (Vector3.Distance(m_target.transform.position, this.transform.position) < 127f)
-            {
-                m_rigid.velocity = Vector3.zero;
-                Debug.Log("Stop");
-                attackTime += Time.deltaTime;
+//        }
+//        else
+//        {
+//            if (Vector3.Distance(m_target.transform.position, this.transform.position) < 127f)
+//            {
+//                m_rigid.velocity = Vector3.zero;
+//                Debug.Log("Stop");
+//                attackTime += Time.deltaTime;
 
-                if (attackTime > m_intarval)
-                {
-                    // ここにEnemyにダメージを与える処理を書く
-                    m_target.GetComponent<IPlayerProp>().Dmage(Attack);
+//                if (attackTime > m_intarval)
+//                {
+//                    // ここにEnemyにダメージを与える処理を書く
+//                    m_target.GetComponent<IPlayerProp>().Dmage(Attack);
 
-                    if (m_target.GetComponent<IPlayerProp>().IsDead)
-                    {
-                        isMovePleyer = false;
-                    }
+//                    if (m_target.GetComponent<IPlayerProp>().IsDead)
+//                    {
+//                        isMovePleyer = false;
+//                    }
                   
 
-                    Debug.Log("Destroy");
-                    attackTime = 0;
-                }
-                return;
-            }
-            //Debug.Log(Vector3.Distance(m_targetpos, this.transform.position));
-            //Debug.Log("ChasePlayer");
-            m_rigid.velocity = (m_target.transform.position - thispos);
+//                    Debug.Log("Destroy");
+//                    attackTime = 0;
+//                }
+//                return;
+//            }
+//            //Debug.Log(Vector3.Distance(m_targetpos, this.transform.position));
+//            //Debug.Log("ChasePlayer");
+//            m_rigid.velocity = (m_target.transform.position - thispos);
            
-        }
-    }
+//        }
+//    }
 
-    public void SetTarget(GameObject tower)
-    {
+//    public void SetTarget(GameObject tower)
+//    {
 
-    }
+//    }
 
    
 
-    public void Damage(int attack)
-    {
-        hp -= attack;
-        m_slider.value -= attack;
+//    public void Damage(int attack)
+//    {
+//        hp -= attack;
+//        m_slider.value -= attack;
     
-        if (hp <= 0) 
-        {
-            GameObject.Find("BattleManager").GetComponent<BattleManager>().AddCost(m_Cost);
-            Destroy(this.gameObject);
-        }
-        //Instantiate(effect, transform.position, Quaternion.identity);
-        Effect().Forget();
-    }
-    async UniTask Effect()
-    {
-        var effects = Instantiate(effect, transform.position, Quaternion.AngleAxis(90, Vector3.right));
-        await UniTask.Delay(TimeSpan.FromSeconds(1));
-        Destroy(effects);
+//        if (hp <= 0) 
+//        {
+//            GameObject.Find("BattleManager").GetComponent<BattleManager>().AddCost(m_Cost);
+//            Destroy(this.gameObject);
+//        }
+//        //Instantiate(effect, transform.position, Quaternion.identity);
+//        Effect().Forget();
+//    }
+//    async UniTask Effect()
+//    {
+//        var effects = Instantiate(effect, transform.position, Quaternion.AngleAxis(90, Vector3.right));
+//        await UniTask.Delay(TimeSpan.FromSeconds(1));
+//        Destroy(effects);
 
-    }
+//    }
 
-    public void OnAtkChanged(int value)
-    {
-        throw new System.NotImplementedException();
-    }
+//    public void OnAtkChanged(int value)
+//    {
+//        throw new System.NotImplementedException();
+//    }
 
-    public void OnHpChanged(int value)
-    {
-        Debug.Log(value);
-        if (hp <= 0) Destroy(this.gameObject);
-    }
-    public  bool trace(Vector3 target, Vector3 self, Rigidbody selfrb, float traceSpeed = 1f)
-    {
-        //if (target)
-        //{
-        //    Debug.Log("Target");
-        //}
-        //else if (self)
-        //{
-        //    Debug.Log("SElf");
-        //}
+//    public void OnHpChanged(int value)
+//    {
+//        Debug.Log(value);
+//        if (hp <= 0) Destroy(this.gameObject);
+//    }
+//    public  bool trace(Vector3 target, Vector3 self, Rigidbody selfrb, float traceSpeed = 1f)
+//    {
+//        //if (target)
+//        //{
+//        //    Debug.Log("Target");
+//        //}
+//        //else if (self)
+//        //{
+//        //    Debug.Log("SElf");
+//        //}
       
-        var direction = (target - self).normalized;
-        if (Vector3.Distance(target, self) < 0.1f)
-        {
-            selfrb.velocity = Vector3.zero;
-            return false;
-        }
-        else selfrb.velocity = direction * traceSpeed;
-        return true;
-    }
-    /// <summary>
-    /// ターゲットとの範囲指定可能追跡機能
-    /// </summary>
-    public  bool trace2(GameObject target, GameObject self, Rigidbody selfrb, float DontMoveAreaRange, float traceSpeed = 1f)
-    {
-        var direction = (target.transform.position - self.transform.position).normalized;
-        if (Vector3.Distance(target.transform.position, self.transform.position) < DontMoveAreaRange)
-        {
-            selfrb.velocity = Vector3.zero;
-            return false;
-        }
-        else selfrb.velocity = direction * traceSpeed;
-        return true;
-    }
-}
+//        var direction = (target - self).normalized;
+//        if (Vector3.Distance(target, self) < 0.1f)
+//        {
+//            selfrb.velocity = Vector3.zero;
+//            return false;
+//        }
+//        else selfrb.velocity = direction * traceSpeed;
+//        return true;
+//    }
+//    /// <summary>
+//    /// ターゲットとの範囲指定可能追跡機能
+//    /// </summary>
+//    public  bool trace2(GameObject target, GameObject self, Rigidbody selfrb, float DontMoveAreaRange, float traceSpeed = 1f)
+//    {
+//        var direction = (target.transform.position - self.transform.position).normalized;
+//        if (Vector3.Distance(target.transform.position, self.transform.position) < DontMoveAreaRange)
+//        {
+//            selfrb.velocity = Vector3.zero;
+//            return false;
+//        }
+//        else selfrb.velocity = direction * traceSpeed;
+//        return true;
+//    }
+//}
